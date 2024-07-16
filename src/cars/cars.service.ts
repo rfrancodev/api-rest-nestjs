e425220be669d85c2a26841b9cf21c1184f04137
rfrancodev <francorfs@icloud.com>
2024-07-15 22:32:06 -0300

    Rotas criadas

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,26 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCarDto } from './dto/create-car.dto';
 import { UpdateCarDto } from './dto/update-car.dto';
+import { Car } from './entities/car.entity';
 
 @Injectable()
 export class CarsService {
+  private readonly cars: Car[] = []
+  private id = 1
+
   create(createCarDto: CreateCarDto) {
-    return 'This action adds a new car';
+    const newCar = {
+      id: this.id,
+      brand: createCarDto.brand,
+      model: createCarDto.model,
+      year: createCarDto.year,
+    }
+    this.id = this.id + 1
+    this.cars.push(newCar)
+
+    return newCar
   }
 
   findAll() {
-    return `This action returns all cars`;
+    return this.cars;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} car`;
+    const car = this.cars.find(car => car.id === id);
+    if (!car) {
+      throw new NotFoundException('Carro não encontrado')
+    }
+    return car;
   }
 
   update(id: number, updateCarDto: UpdateCarDto) {
-    return `This action updates a #${id} car`;
+    const car = this.findOne(id)
+
+    car.brand = updateCarDto.brand
+    car.model = updateCarDto.model
+    car.year = updateCarDto.year
+
+    return;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} car`;
+    this.findOne(id)
+
+    const carIndex =  this.cars.findIndex(car => car.id === id);
+
+    this.cars.splice(carIndex, 1)
+
+    return;
   }
 }
